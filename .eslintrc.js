77f09fd366ca0486a563dbee8dea813f398fc2a4
AbdelrahmanSherf <66575300+AbdelrahmanSherf@users.noreply.github.com>
2021-12-21 09:46:11 -0500

    chore: dependencies required for this project, Express, TypeScript, Jasmine, Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true
+    },
+    extends: [
+        'eslint:recommended',    
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        'ecmaVersion': 13,
+        'sourceType': 'module'
+    },
+    plugins: [
+        '@typescript-eslint', 
+        'prettier'
+    ],
+    rules: {
+        'prettier/prettier': 2, // It means Error
+    }
+};
+
+
+// module.exports = {
+//     env: {
+//         es2021: true,
+//       node: true
+//     },
+//    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+//     parser: '@typescript-eslint/parser',
+//     parserOptions: {
+//       ecmaVersion: 13,
+//       sourceType: 'module'
+//     },
+//    plugins: ['@typescript-eslint', 'prettier'],
+//     rules: {
+//      'prettier/prettier': 2, // Means error
+//     }
+//   }
+  
