163ab747b3620b993d436cdbf8244f0ab2780827
AbdelrahmanSherf <66575300+AbdelrahmanSherf@users.noreply.github.com>
2021-12-24 04:13:06 -0500

    refactor: some code refactor, minor changes

--- src/functionality/resizeFunc.ts
+++ src/functionality/resizeFunc.ts
@@ -1,21 +1,29 @@
-import sharp from 'sharp';
-import fse from 'fs-extra';
-import exp from 'constants';
+import sharp from 'sharp'
 
 // resize image with sharp
-const resizeImage = async(width: number, height: number, originalImagePath: string, resizedImagePath: string) => {
-    try {
-        const processedImage = sharp(originalImagePath).resize({
-            width: width,
-            height: height
-        });
-        await processedImage.toFile(resizedImagePath);
-        console.log('Image has been successfually resized');
-        return resizedImagePath;
-    } catch {
-        console.error('ERROR LOCACATION: resizeFunc Module, error resizing the image');
-        return 'ERROR Resizing your image';
-    }
+const resizeImage = async (
+  width: number,
+  height: number,
+  originalImagePath: string,
+  resizedImagePath: string
+) => {
+  try {
+    const processedImage = sharp(originalImagePath).resize({
+      width: width,
+      height: height
+    })
+    // await and send to resized folder
+    await processedImage.toFile(resizedImagePath)
+    console.log('Image has been successfually resized')
+    // console.log('debugging','typeof:',typeof resizedImagePath, resizedImagePath); // Debugging
+    return resizedImagePath
+  } catch (err) {
+    console.error(
+      'ERROR LOCACATION: resizeFunc Module, error resizing the image'
+    )
+    // return 'ERROR Resizing your image';
+    return `ERROR Resizing your image, ${err}`
+  }
 }
 
-export default resizeImage;
+export default resizeImage
